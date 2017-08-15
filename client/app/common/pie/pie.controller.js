1673774e1839b51597897da3d4a851f938fa8c8f
volodya-buryy <vova.buryy@yandex.com>
2017-08-15 18:23:28 +0300

    pie with colors

--- client/app/common/pie/pie.controller.js
+++ client/app/common/pie/pie.controller.js
@@ -19,20 +19,19 @@ class pieDirective {
 
         scope.$watch('values', function(values) {
             if(values) {
+                const n = 7;
                 console.log('values from directive: ', values);
 
                 var width = 960,
-                height = 500,
-                radius = Math.min(width, height) / 2;
+                    height = 500,
+                    radius = Math.min(width, height) / 2;
+                var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-                var color = d3.scale.ordinal()
-                    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-                var arc = d3.svg.arc()
+                var arc = d3.arc()
                     .outerRadius(radius - 10)
                     .innerRadius(0);
 
-                var pie = d3.layout.pie()
+                var pie = d3.pie()
                     .sort(null)
                     .value(function(d) {
                         return d.population;
@@ -43,7 +42,6 @@ class pieDirective {
                     .attr("height", height)
                     .append("g")
                     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
 
 
                     values.forEach(function(d) {
@@ -57,13 +55,20 @@ class pieDirective {
 
                     g.append("path")
                         .attr("d", arc)
-                        .style("fill", function(d) { return color(d.data.age); });
+                        .style("fill", function(d) {
+                            console.log(color(d.data.age), "color")
+                            return color(d.data.age);
+                        });
 
                     g.append("text")
-                        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+                        .attr("transform", function(d) {
+                            return "translate(" + arc.centroid(d) + ")";
+                        })
                         .attr("dy", ".35em")
                         .style("text-anchor", "middle")
-                        .text(function(d) { return d.data.age; });
+                        .text(function(d) {
+                            return d.data.age;
+                        });
 
             }
         })
