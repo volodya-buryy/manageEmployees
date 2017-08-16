5a398e2c15623b6f5b59899cc109c25e0cefd1e1
volodya-buryy <vova.buryy@yandex.com>
2017-08-16 21:24:06 +0300

    table

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -6,28 +6,37 @@ class HomeController {
 				name: "John",
 				age: "23",
 				skill: "js",
-				level: "3"
+				level: 3
 			},
 			{
 				name: "Dou",
 				age: "32",
 				skill: "php",
-				level: "2"
+				level: 2
 			},
 			{
 				name: "John",
 				age: "23",
 				skill: "php",
-				level: "2"
+				level: 2
 			},
 			{
 				name: "Dou",
 				age: "3",
 				skill: "js",
-				level: "2"
+				level: 2
 			},
 		];
 	};
+
+	removeItem(index) {
+		this.model.splice(index, 1);
+	}
+
+	editItem(index){
+		this.editEvent = true
+		console.log(this.model[index]);
+	}
 };
 
 export default HomeController;
